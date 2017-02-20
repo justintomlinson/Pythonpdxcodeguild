2b1007532bbe1560ee0be8b2005f9deb129ce3c4
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-02-20 15:56:02 -0800

    first Commit

--- code1/test.py
+++ code1/test.py
@@ -1,1 +1,2 @@
 print("This is a test")
+# NOTE: I am writing a comments
--- code1/volumconverter.py
+++ code1/volumconverter.py
@@ -1,3 +1,5 @@
+******* Converts Gallons to Liters ******
 gallon_num = float(input("Enter volume in gallons"))
 liter_num = round(float(gallon_num*3.78541),2)
 print (str(gallon_num) +"gallons ="+ str(liter_num) + "liters")
+print (test)
