488cad3c447b99972f6e15e75a0300f160fc9116
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-02-23 13:21:13 -0800

    Commit pig-latin advance work

--- code1/pig-latin.py
+++ code1/pig-latin.py
@@ -6,27 +6,33 @@ suf_vowel = "yay"
 # string to attach to the end of a word that begins with a consonant
 suf_consonant = "ya"
 #list of vowels
-vowel_list = ["a","e","i","o","u"]
+vowel_list = ["a","e","i","o","u", "A","E","I","O", "U"]
 # Establish variable for pig latin word
 pig_latin = 0
-
+#punc_list =[".","?", "!", ",", ":", ";"]
 #identifies first letter of inputed word
 first_letter = orig_word[0]
+punc_last = orig_word[-1]
+
+# Identifies if input work contants an non alpha-numeric and removes it from the end
+if orig_word.isalpha() == False:
+    orig_word = orig_word[:len(orig_word)-1]
 
 # Coverts the english word to pig latin
-# Looks for first letter in the list of vowels. If true adds the "yay" to input word,
-# if false moves the first letter to end of the orginal string and adds "ya"
+# Looks for first letter in the list of vowels to determine if the word starts with a vowel or consonant.
+# Then determines whether the first letter is capitalized. Then arranges the word into pig latin, capitalizing
+# the first letter if necessary bassed on original input.
 if  first_letter in vowel_list:
     (first_letter == first_letter.upper())== True
-    pig_latin = "".join(first_letter +orig_word[1:]+ suf_vowel)
+    pig_latin = "".join(orig_word+ suf_vowel+ punc_last)
     print(orig_word + " in pig latin is " + pig_latin)
 elif first_letter in vowel_list:
-    pig_latin = orig_word+suf_vowel
+    pig_latin = orig_word+suf_vowel+ punc_last
     print(orig_word + " in pig latin is " + pig_latin)
 elif first_letter == first_letter.upper():
-     pig_latin = "".join(orig_word[1].upper() + orig_word[1:] + first_letter.lower() + suf_consonant)
+     pig_latin = "".join(orig_word[1].upper() + orig_word[2:] + first_letter.lower() + suf_consonant+ punc_last)
      print(orig_word + " in pig latin is " + pig_latin)
 else:
-    pig_latin = "".join(orig_word[1:] + first_letter + suf_consonant)
+    pig_latin = "".join(orig_word[1:] + first_letter + suf_consonant+ punc_last)
     print(orig_word + " in pig latin is " + pig_latin)
 
