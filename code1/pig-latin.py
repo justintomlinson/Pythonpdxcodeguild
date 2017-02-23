d54cf79cc90bb7dbcdd5eedbdf17e9116c5679cb
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-02-23 12:04:09 -0800

    Commit pig-latin advance work

--- code1/pig-latin.py
+++ code1/pig-latin.py
@@ -16,20 +16,16 @@ first_letter = orig_word[0]
 # Coverts the english word to pig latin
 # Looks for first letter in the list of vowels. If true adds the "yay" to input word,
 # if false moves the first letter to end of the orginal string and adds "ya"
-if first_letter == first_letter.upper():
-    first_letter in vowel_list == True
+if  first_letter in vowel_list:
+    (first_letter == first_letter.upper())== True
     pig_latin = "".join(first_letter +orig_word[1:]+ suf_vowel)
-    print(orig_word + " in pig latin is " + pig_latin)
-elif first_letter not in vowel_list
-    pig_latin = "".join(orig_word[1:].upper() + first_letter.lower() + suf_consonant):
     print(orig_word + " in pig latin is " + pig_latin)
 elif first_letter in vowel_list:
     pig_latin = orig_word+suf_vowel
     print(orig_word + " in pig latin is " + pig_latin)
-# elif first_letter != vowel_list:
-#     first_letter == first_letter.upper()
-#     pig_latin = "".join(orig_word[1:].upper() + first_letter.lower() + suf_consonant)
-#     print(orig_word + " in pig latin is " + pig_latin)
+elif first_letter == first_letter.upper():
+     pig_latin = "".join(orig_word[1].upper() + orig_word[1:] + first_letter.lower() + suf_consonant)
+     print(orig_word + " in pig latin is " + pig_latin)
 else:
     pig_latin = "".join(orig_word[1:] + first_letter + suf_consonant)
     print(orig_word + " in pig latin is " + pig_latin)
