f996a41e79f361c2f7f9eb25db4424d1cf561953
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-02-24 13:57:22 -0800

    Commit refactored distance-converter

--- code1/distance-converter.py
+++ code1/distance-converter.py
@@ -1,49 +1,55 @@
-orig_dist = float(input("Enter a distance > "))
-orig_unit = input("Enter units (mi, km, ft and m) >")
-target_unit = input("Enter target units (mi, km, ft, and m) >")
-target_dist = 0
+"""This program converts between a set unit of distance"""
 
-miles_km = 1.60934
-miles_m = 160934
-miles_ft = 5280
-km_m = 1000
-m_ft = 3.28084
-km_ft = m_ft*km_m
+def convert_to_meters (orig_dist,orig_unit):
+    miles_m = orig_dist*1609.34
+    km_m = orig_dist*1000
+    ft_m = orig_dist*3.28084
+    in_m = orig_dist*0.254
+    cm_m = orig_dist/100
 
-if orig_unit == "mi":
-    if target_unit == "km":
-        target_dist = orig_dist*miles_km
-    elif target_unit == "m":
-        target_dist = orig_dist*miles_m
+    if orig_unit == "ft":
+        meter_dist = ft_m
+    elif orig_unit == "mi":
+        meter_dist = miles_m
+    elif orig_unit == "km":
+        meter_dist = km_m
+    elif orig_unit == "in":
+        meter_dist = in_m
+    elif orig_unit == "cm":
+        meter_dist = cm_m
     else:
-        target_unit == "ft"
-        target_dist = orig_dist*miles_ft
+        meter_dist = orig_dist
+    return meter_dist
 
-if orig_unit == "km":
+def convert_from_meter(target_unit, meter_dist):
+    m_miles = meter_dist/1609.34
+    m_km = meter_dist/1000
+    m_ft = 3.28084/meter_dist
+    m_in = meter_dist/0.254
+    m_cm = meter_dist*100
+    convert_dist = 0
     if target_unit == "mi":
-        target_dist = orig_dist/miles_km
-    elif target_unit == "m":
-        target_dist = orig_dist*km_m
-    else:
-        target_unit == "ft"
-        target_dist = orig_dist*km_ft
-
-if orig_unit == "m":
-    if target_unit == "mi":
-        target_dist = orig_dist/miles_m
+        convert_dist = m_miles
     elif target_unit == "km":
-        target_dist = orig_dist/km_m
+        convert_dist = m_km
+    elif target_unit == "ft":
+        convert_dist = m_ft
+    elif target_unit == "in":
+        convert_dist = m_in
+    elif target_unit == "cm":
+        convert_dist = m_cm
     else:
-        target_unit == "ft"
-        target_dist = orig_dist*(km_ft/km_m)
+        convert_dist = meter_dist
+    return round(convert_dist,5)
 
-if orig_unit == "ft":
-    if target_unit == "mi":
-        target_dist = orig_dist/miles_ft
-    elif target_unit == "km":
-        target_dist = orig_dist/km_ft
-    else:
-        target_unit == "m"
-        target_dist = orig_dist/m_ft
-print(str(orig_dist)+ " "+ str(orig_unit)+ " is " +  str(target_dist)+" " + str(target_unit))
+def main ():
+    orig_dist = float(input("Enter a distance > "))
+    orig_unit = input("Enter units (mi, km, ft, in, cm and m) >")
+    target_unit = input("Enter target units (mi, km, ft, in, cm and m) >")
 
+    meter_dist = convert_to_meters(orig_dist, orig_unit)
+    convert_dist = convert_from_meter(target_unit,meter_dist)
+    print(str(orig_dist)+ str(orig_unit) + " is " + str(convert_dist)+ str(target_unit))
+   # print ("%g %s is %g %s" %(orig_dist, orig_unit, convert_dist, target_unit))
+if __name__ == '__main__':
+    main()
