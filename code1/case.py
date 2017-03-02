dead1df75d35f5b4312c76bd8e597c977088222f
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-03-01 16:53:55 -0800

    Commit more code - not tested

--- code1/case.py
+++ code1/case.py
@@ -2,19 +2,16 @@
 
 def define_case(user_input):
     """This function defines the case of the user's input
-    >>>define_case('snake_case')
+    >>> define_case('snake_case')
 
-    >>>define_case('CamelCase')
+    >>> define_case('CamelCase')
 
-    >>>define_case('kebab-case')
+    >>> define_case('kebab-case')
 
-    >>>define_case('CONSTANT_CASE')
+    >>> define_case('CONSTANT_CASE')
 
     :return:
     """
-
-    user_input = str(input('Please provide a word in snake_case, CamelCase, kebab-case, or CONSTANT_CASE ->'))
-
     case_type = ''
 
     if user_input.islower():
@@ -30,49 +27,65 @@ def define_case(user_input):
 
 def case_change_intermediate(user_input, case_type):
     """This function changes the inputed word into a agnostic representation to be worked on later
-    >>>case_change_intermediate('snake_case')
+    >>> case_change_intermediate('snake_case')
 
-    >>>case_change_intermediate('CamelCase')
+    >>> case_change_intermediate('CamelCase')
 
-    >>>case_change_intermediate('kebab-case')
+    >>> case_change_intermediate('kebab-case')
 
-    >>>case_change_intermediate('CONSTANT_CASE')
+    >>> case_change_intermediate('CONSTANT_CASE')
 
 
     :return:
     """
     working = user_input.copy()
+    new_list = []
+    inc = 0
 
     if case_type == 'snake':
         split = working.split("_")
     elif case_type == 'kebab':
         split = working.split("-")
     elif case_type == 'constant':
-        split = working.split("-")
+        split = (working.lower()).split("-")
     else:
-       for index, char in enumerate(working):
-           print(index)
-           print(char)
-
-
+        for index, char in enumerate(working[1:], 1):
+            if char.isupper():
+                new_list.append(working[inc:index])
+                inc = index
+        if inc < len(working):
+            new_list.append(working[inc:])
+        split = new_list.lower
 
     return (split)
 
-def case_change_final(x, y):
+def case_change_final(split,new_format):
     """This function converts an agnostic version of the word from case_change_intermediate to the new desired case
     format.
-    >>>case_change_final('snake_case')
+    >>> case_change_final('snake_case')
 
-    >>>case_change_final('CamelCase')
+    >>> case_change_final('CamelCase')
 
-    >>>case_change_final('kebab-case')
+    >>> case_change_final('kebab-case')
 
-    >>>case_change_final('CONSTANT_CASE')
+    >>> case_change_final('CONSTANT_CASE')
 
 
     :return:
     """
-    split =
+    case_change = ''
+
+    if new_format = 'camel':
+        case_change = split.join(split[0].capitalize() + split[1].capitalize())
+    elif new_format = 'snake':
+        case_change = split.join(split[0]+'_'+split[1])
+    elif new_format ='kebab':
+        case_change = split.join(split[0]+'-'+split[1])
+    else:
+        case_chagne = split.join(split[0]+'-'+split[1])
+
+    
+
     return
 
 def output_case():
@@ -90,10 +103,9 @@ def output_case():
     return
 
 
-
-# def main ():
-#   user_input = str(input('Please provide a word in snake_case, CamelCase, kebab-case, or CONSTANT_CASE ->'))
+ def main ():
+   user_input = str(input('Please provide a word in snake_case, CamelCase, kebab-case, or CONSTANT_CASE ->'))
     new_format = str
-# if __name__ == '__main__':
+ if __name__ == '__main__':
 #     main()
 
