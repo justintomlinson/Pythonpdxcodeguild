5bd42d62136ad495e024dd4ca7fde926f29cf106
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-03-04 18:25:30 -0800

    Commit - updates to table pretty print - List now in correct format

--- code1/table-pretty-print.py
+++ code1/table-pretty-print.py
@@ -7,53 +7,41 @@
 #    """
 import csv
 
-results= []
-#with open('inputfile.txt', newline ='') as inputfile:
-#    for row in csv.reader(inputfile):
-#        results.append(row)
 
-# with open('inputfile.txt',newline ='') as inputfile:
-#     results = list(csv.reader(inputfile))
-# print(results)
-# # #    return
 
 with open('inputfile.txt') as input_data:
     file_lines = input_data.readlines()
 
-new = []
-new2 =[]
-print(file_lines)
+
+new_list = []
 for index, value in enumerate(file_lines):
-       for i, elem in enumerate(value):
-           new.append(((value.strip('\n')).split(' ')))
-       print(new)
-new_mod = []
+    new_list.append((value.strip('\n')).split(','))
+#
+work_list = []
+for sublst in new_list:
+    for item in sublst:
+        work_list.append(','.join(sublst).split(' '))
+
+max_length = [max(len(str(x))for x in column)for column in zip(*work_list)]
 
 
-
-#print(new_mod1)
-
-
-max_length = [max(len(str(x))for x in column)for column in zip(*new)]
-print(max_length)
-
-# for index, elem in enumerate(new):
-#     if index != len(new):
-#         new_mod.append((elem).split(','))
-
-print(new_mod)
-# def make_partition_line(max_length):
+# # for index, elem in enumerate(new):
+# #     if index != len(new):
+# #         new_mod.append((elem).split(','))
+#
+# # def make_partition_line(max_length):
 for i, col in enumerate(max_length):
     make_line = str('|'+('-'*max_length[i]))
 #    return(make_line)
 
-#insert_line = (str('|'+'-'*col_len +'|', end = ''))
-print('|')
-for index, value in enumerate(new):
-    if index < 2:
-        print((make_line), end = '')
-        for i, item in enumerate(value):
-            print(make_line, end = '',)
+#insert_line = (str('|'+'-'*max_length +'|', end = ''))
+print('|'+ make_line)
+for index, value in enumerate(work_list):
+    print(value)
+     if index < 2:
+         print(work_list + make_line, end = '')
+         for i, item in enumerate(value):
+             print(make_line, end = '',)
 
 print('|')
 
@@ -64,10 +52,3 @@ print('|')
 
 
 
-# for index, char in enumerate(working[1:], 1):
-#     if char.isupper():
-#         new_list.append(working[inc:index])
-#         inc = index
-# if inc < len(working):
-#     new_list.append(working[inc:])
-# split = [x.lower() for x in new_list]
