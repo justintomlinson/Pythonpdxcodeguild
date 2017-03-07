f9a295a630de911385afa0446efe871133f317fd
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-03-06 16:36:41 -0800

    Connect Four - end of day

--- code1/connect-four.py
+++ code1/connect-four.py
@@ -10,34 +10,50 @@ with open('connect-four-moves.txt') as input_data:
 move_2 = []
 
 for index, value in enumerate(move_list):
-    move_2.append(value.strip('\n'))
+    move_2.append(value.strip('\n').split())
     for x, elem in enumerate(move_2):
-     move_2[x].split(' ')
+     ','.join(elem)
 
-print(move_2)
+#print(move_2)
 #   return
 
 
-#def change_move_list(move_list)
+#def change_move_list(move_2)
 #   append piece color to move list
 # yellow goes first
 move_update = []
-
+y = ['y']
+r = ['r']
 for i, val in enumerate(move_2):
     if i % 2 == 0:
-        move_update.append(val +'y')
+        move_update.append(val + y )
     else:
-        move_update.append(val + 'r')
-
+        move_update.append(val + r)
 
 print (move_update)
 
 #   return
 
-
-
-
-
 #def build_board(row, col)
 
-#def add_move_to_board
+rows = 6
+col = 7
+
+game_board = [[]*col for i in range(rows)]
+print(game_board)
+print(len(game_board))
+#   return()
+
+#def add_move_to_board(game_board, move_update)
+
+game_play =[]
+for index, list in enumerate(range(len(game_board)- 1,0,-1)):
+    for col, color in enumerate(move_update):
+        game_board.insert[:color]
+
+print(game_board)
+
+
+
+
+
