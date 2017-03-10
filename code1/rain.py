6f524a875f3c62a096b74add574971c538eb5597
Justin Tomlinson <jet.tomlinson@gmail.com>
2017-03-10 11:36:59 -0800

    Add rain.py to Branch

--- code1/rain.py
+++ code1/rain.py
@@ -15,14 +15,12 @@ for i,line in enumerate(rain_list):
 for data in rain_list[11:len(rain_list)]: # you cheated fix this later
         just_data.append(data.split())
 
-data_splitdate =[]
-for i in range(0,len(just_data)):
-    just_data[i][0].split('-')
+#data_splitdate =[]
+#for i in range(0,len(just_data)):
+#    just_data[i][0].split('-')
 
-
-
-rain_dict = {d[0]:d[1]for d in just_data}
-
+rain_dict = {d[0]:int(d[1])for d in just_data}
+print(rain_dict)
 
 #def find_most_rain by_date(rain_dict)
 max_rain = max(rain_dict.items(), key=lambda k: k[1])
@@ -31,9 +29,48 @@ print(max_rain)
 
 
 #def find_year_with_most_rain(rain_dict):
-import datetime
-date_format = '%d-%m-%Y'
-x= rain_dict
-for i in x:
-    datetime= datetime.datetime.strptime(x.keys(),date_format)
+# import datetime
+# date_format = '%d-%m-%Y'
+# x= rain_dict
+# for i in x:
+#     datetime= datetime.datetime.strptime(x.keys(),date_format)
+
+
+# list_rain_dict = list(rain_dict.items())
 #
+# for item in list_rain_dict:
+#     for key, value in item.iteritems():
+#         item[key] = int(value)
+# print(list_rain_dict)
+#
+# # r_dict_mod = [dict([a, int(x)] for a, x in rain_dict.iteritems())for ]
+# # for x in rain_dict.values():
+# #     print(int(rain_dict.values()))
+#
+#def group_by(iterable, key):
+#    """Place each item in an iterable into a bucket based on calling the key
+#    function on the item."""
+group_to_items = {}
+for item in rain_dict:
+   group = item[7:11]
+   if group not in group_to_items:
+      group_to_items[group] = []
+      group_to_items[group].append(item)
+#return group_to_items
+print(group_to_items)
+
+#for x, y in rain_dict.items():
+
+#print(year)
+#         yr16 = sum(rain_dict.values())
+#     elif x[7:11] == '2015':
+#         yr15 = sum(rain_dict.values())
+#     elif x[7:11] == '2014':
+#         yr14 = sum(rain_dict.values())
+#     elif x[7:11] == '2013':
+#         yr13 = sum(rain_dict.values())
+#     elif x[7:11] == '2012':
+#         yr12 = sum(rain_dict.values())
+#
+# print (str(yr16))
+# print (str(yr15))
